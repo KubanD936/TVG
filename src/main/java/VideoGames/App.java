14e23266ddf66624ca2bc36d39ef9011ba5f878c
KubanD936 <kuban627@revature.net>
2022-03-01 12:16:19 -0500

    Removed old ArrayList since I couldn't make it work with server. Added Tomcat, maybe working yet. Added h2 database, haven't checked if it works yet. Still need to add tests.

--- src/main/java/VideoGames/App.java
+++ src/main/java/VideoGames/App.java
@@ -2,14 +2,6 @@ package VideoGames;
 
 import org.apache.catalina.LifecycleException;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Objects;
-
 public class App {
     public static void main(String[] args) {
         GameContext.build();
